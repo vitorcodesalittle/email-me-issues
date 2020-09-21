d3a0a5a96222f35dd1eaf08ef9d7427b93f0a1af
Vitor Maia <vlma@cin.ufpe.br>
2020-09-21 11:01:54 -0300

    emailing issues in ugly format

--- email-issues.py
+++ email-issues.py
@@ -8,7 +8,8 @@ import sched
 import time
 from bs4 import BeautifulSoup
 from datetime import datetime
-
+import smtplib
+from email.message import EmailMessage
 # https://docs.python.org/3/library/sched.html
 s = sched.scheduler(time.time, time.sleep)
 
@@ -32,6 +33,9 @@ def build_issues_url(url, tag):
   else:
     return url + "/issues"
 
+def build_single_issue_url(url, issue_id):
+  return url + '/issues/' + issue_id
+
 def get_issues_html(url):
   return requests.get(url).content
 
@@ -42,6 +46,28 @@ def extract(str, regex):
     return str[s[0]:s[1]]
   else:
     return None
+
+def send_email(from_email, from_password, smtp_server,smtp_port, to_email, content, subject):
+  me = from_email
+  you = to_email
+  s = smtplib.SMTP(smtp_server, port=smtp_port)
+  s.ehlo()
+  s.starttls()
+  s.login(from_email, from_password)
+  msg = EmailMessage()
+  msg.set_content(content)
+
+  # me == the sender's email address
+  # you == the recipient's email address
+  msg['Subject'] = subject
+  msg['From'] = me
+  msg['To'] = you
+  # Send the message via SMTP server.
+  try:
+    s.send_message(msg)
+    s.quit()
+  except Exception as e:
+    raise e
 
 def get_recent_issues(issues_html, from_data):
   soup = BeautifulSoup(issues_html, 'html.parser')
@@ -61,6 +87,12 @@ config_raw = io.open(config_path, mode="r")
 config= json.loads(config_raw.read())
 repos = config['repos']
 timeout = config['interval']
+from_email = config['sender-email']
+to_email = config['to-email']
+from_password = config['sender-password']
+server = config['smtp-server']
+port = config['server-port']
+subject = "New issues found!"
 
 for repo in repos:
   link = repo['link']
@@ -69,12 +101,16 @@ for repo in repos:
 
 def run():
   global last_time_checked
+  content = ''
   for repo in repos:
     for url in repo['urls']:
       html = get_issues_html(url)
-      issues = get_recent_issues(html, datetime.now())
-      print("NEW ISSUES OF {}: ".format(repo), issues)
+      issues = get_recent_issues(html, last_time_checked)
+      if (len(issues) > 0):
+        content = content + "\nNEW ISSUES OF {}: {}".format(repo, ', '.join(map(lambda issue_id: build_single_issue_url(url, issue_id), issues)))
   last_time_checked = datetime.now()
+  if (content !=''):
+    send_email(from_email, from_password, server, port, to_email, content, subject)
   s.enter(timeout, 1, run)
 
 s.enter(timeout, 1, run)
